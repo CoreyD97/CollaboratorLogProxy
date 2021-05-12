37b8ceb1b8291285e2b30fe7a1b4fb3fc24e1eba
Corey-Dean Arthur <1339555+CoreyD97@users.noreply.github.com>
2021-05-12 17:48:32 +0100

    Switch to commons config, remove debug code, add rolling log option

--- src/main/java/com/nccgroup/collaboratorlogproxy/CollaboratorLogProxy.java
+++ src/main/java/com/nccgroup/collaboratorlogproxy/CollaboratorLogProxy.java
@@ -1,6 +1,11 @@
 package com.nccgroup.collaboratorlogproxy;
 
-import nu.studer.java.util.OrderedProperties;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.conn.ssl.TrustAllStrategy;
 import org.apache.http.impl.NoConnectionReuseStrategy;
 import org.apache.http.impl.bootstrap.HttpServer;
 import org.apache.http.impl.bootstrap.ServerBootstrap;
@@ -8,20 +13,26 @@ import org.apache.http.ssl.SSLContexts;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.appender.RollingFileAppender;
+import org.apache.logging.log4j.core.appender.rolling.SizeBasedTriggeringPolicy;
+import org.apache.logging.log4j.core.appender.rolling.TimeBasedTriggeringPolicy;
+import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 import java.util.ArrayList;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class CollaboratorLogProxy {
@@ -33,23 +44,22 @@ public class CollaboratorLogProxy {
     private static final String COLLABORATOR_HTTPS_SERVER_PORT = "collaborator_https_server_port";
     private static final String LOG_PROXY_HTTP_SERVER_PORT = "log_proxy_http_server_port";
     private static final String LOG_PROXY_HTTPS_SERVER_PORT = "log_proxy_https_server_port";
-    private static final String LISTEN_ADDRESS = "listen_address";
     private static final String PRIVATE_KEY_PATH = "ssl_private_key_path";
     private static final String CERTIFICATE_PATH = "ssl_certificate_path";
     private static final String INTERMEDIATE_CERTIFICATE_PATH = "ssl_intermediate_certificate_path";
     private static final String INTERMEDIATE_CERTIFICATE_DEFAULT = "/certs/intermediate.crt";
     private static final String LOG_LEVEL = "log_level";
+    private static final String LOG_DIR = "log_directory";
+    private static final String ROLLING_LOGS = "rolling_logs";
 
     private HttpServer httpServer;
     private HttpServer httpsServer;
 
-    private CollaboratorLogProxy(Properties properties) throws Exception {
-        Level logLevel = Level.valueOf(properties.getProperty(LOG_LEVEL));
-        Configurator.setRootLevel(logLevel);
+    private CollaboratorLogProxy(org.apache.commons.configuration2.Configuration properties) throws Exception {
 
-        String actualAddress = properties.getProperty(COLLABORATOR_SERVER_ADDRESS);
-        Integer actualHTTPPort = Integer.parseInt(properties.getProperty(COLLABORATOR_HTTP_SERVER_PORT));
-        Integer actualHTTPSPort = Integer.parseInt(properties.getProperty(COLLABORATOR_HTTPS_SERVER_PORT));
+        String actualAddress = properties.getString(COLLABORATOR_SERVER_ADDRESS);
+        Integer actualHTTPPort = properties.getInt(COLLABORATOR_HTTP_SERVER_PORT);
+        Integer actualHTTPSPort = properties.getInt(COLLABORATOR_HTTPS_SERVER_PORT);
 
         InetAddress listenAddress = InetAddress.getByName("0.0.0.0");
 
@@ -58,15 +68,15 @@ public class CollaboratorLogProxy {
         SSLContext sslContext;
         //Load private key
         logger.info("Loading private key from file: " + properties.getProperty(PRIVATE_KEY_PATH));
-        PrivateKey privateKey = Utilities.loadPrivateKeyFromFile(properties.getProperty(PRIVATE_KEY_PATH));
+        PrivateKey privateKey = Utilities.loadPrivateKeyFromFile(properties.getString(PRIVATE_KEY_PATH));
 
         ArrayList<Certificate> certificateList = new ArrayList<>();
         //Load certificate
         logger.info("Loading certificate from file: " + properties.getProperty(CERTIFICATE_PATH));
-        certificateList.add(Utilities.loadCertificateFromFile(properties.getProperty(CERTIFICATE_PATH)));
+        certificateList.add(Utilities.loadCertificateFromFile(properties.getString(CERTIFICATE_PATH)));
         //Load intermediate certificate
-        String intermediatePath = properties.getProperty(INTERMEDIATE_CERTIFICATE_PATH);
-        if(!intermediatePath.equals("") && !intermediatePath.equals(INTERMEDIATE_CERTIFICATE_DEFAULT)){
+        String intermediatePath = properties.getString(INTERMEDIATE_CERTIFICATE_PATH);
+        if(!StringUtils.isEmpty(intermediatePath) && !intermediatePath.equals(INTERMEDIATE_CERTIFICATE_DEFAULT)){
             logger.info("Loading intermediate certificate from file: " + properties.getProperty(INTERMEDIATE_CERTIFICATE_PATH));
             certificateList.add(Utilities.loadCertificateFromFile(intermediatePath));
         }
@@ -82,28 +92,20 @@ public class CollaboratorLogProxy {
 
         httpServer = ServerBootstrap.bootstrap()
                 .setConnectionReuseStrategy(new NoConnectionReuseStrategy())
-                .setListenerPort(Integer.parseInt(properties.getProperty(LOG_PROXY_HTTP_SERVER_PORT)))
+                .setListenerPort(properties.getInt(LOG_PROXY_HTTP_SERVER_PORT))
                 .setLocalAddress(listenAddress)
                 .setExceptionLogger(ex -> {
-                    if(ex instanceof SSLException){
-                        logger.error("Client Connection Failed: " + ex.getMessage());
-                    }else{
-                        logger.error(ex);
-                    }
+                    logger.debug(ex);
                 })
                 .registerHandler("*", new HttpHandler(actualAddress, actualHTTPPort, false)).create();
 
 
         httpsServer = ServerBootstrap.bootstrap()
                 .setConnectionReuseStrategy(new NoConnectionReuseStrategy())
-                .setListenerPort(Integer.parseInt(properties.getProperty(LOG_PROXY_HTTPS_SERVER_PORT)))
+                .setListenerPort(properties.getInt(LOG_PROXY_HTTPS_SERVER_PORT))
                 .setLocalAddress(listenAddress)
                 .setExceptionLogger(ex -> {
-                    if(ex instanceof SSLException){
-                        logger.error("Client Connection Failed: " + ex.getMessage());
-                    }else{
-                        logger.error(ex);
-                    }
+                    logger.debug(ex);
                 })
                 .setSslContext(sslContext)
                 .registerHandler("*", new HttpHandler(actualAddress, actualHTTPSPort, true)).create();
@@ -124,12 +126,14 @@ public class CollaboratorLogProxy {
     }
 
     private SSLContext createSSLContext(final KeyStore keyStore, final char[] password) throws Exception {
-        return SSLContexts.custom().loadKeyMaterial(keyStore, password).build();
+        return SSLContexts.custom().loadKeyMaterial(keyStore, password).loadTrustMaterial(new TrustAllStrategy()).build();
     }
 
     public static void main(String[] args) throws Exception {
 
-        OrderedProperties properties = getDefaultProperties();
+        FileBasedConfigurationBuilder<PropertiesConfiguration> configBuilder;
+        org.apache.commons.configuration2.Configuration config;
+
         if(args.length == 0){
             //Create default properties file
             File defaultsFile = new File("LogProxy.properties");
@@ -139,13 +143,13 @@ public class CollaboratorLogProxy {
                         " or remove the file to allow it to be populated with the defaults");
                 return;
             }
-            FileOutputStream outputStream = new FileOutputStream(defaultsFile);
-            properties.store(outputStream, "MAKE SURE THE SECRET IS CHANGED TO SOMETHING MORE SECURE!\n" +
-                    "By default, the private key and certificates will be used to\n" +
-                    "configure the SSL context. To use a keystore instead, comment out " + PRIVATE_KEY_PATH + ".");
-            logger.info("Default config written to " + defaultsFile.getName());
-            logger.info("Edit the config (especially the secret!)");
-            logger.info("Then start the server with `java -jar CollaboratorAuth.jar " + defaultsFile.getName() + "`");
+
+            InputStream defaultsResource = CollaboratorLogProxy.class.getClassLoader().getResourceAsStream("LogProxy.properties");
+            Files.copy(defaultsResource, Path.of(defaultsFile.toURI()));
+
+
+            logger.warn("Default config written to " + defaultsFile.getName());
+            logger.warn("Edit the config, then start the server with `java -jar CollaboratorAuth.jar " + defaultsFile.getName() + "`");
             return;
         }else{
             File configFile = new File(args[0]);
@@ -153,34 +157,52 @@ public class CollaboratorLogProxy {
                 logger.error("Config file does not exist. Run the jar without arguments to generate the default config.");
                 return;
             }else{
-                try (FileInputStream inputStream = new FileInputStream(configFile)) {
-                    properties.load(inputStream);
+                configBuilder = new Configurations().propertiesBuilder(configFile);
+                config = configBuilder.getConfiguration();
+
+                //Setup log file
+                Level logLevel = Level.valueOf(config.getString(LOG_LEVEL));
+                Configurator.setRootLevel(logLevel);
+
+                LoggerContext context = (LoggerContext) LogManager.getContext(false);
+                Configuration logConfig = context.getConfiguration();
+
+                PatternLayout logLayout = PatternLayout.newBuilder()
+                        .withConfiguration(logConfig)
+                        .withPattern("[%-5level] %d{yyyy-MM-dd HH:mm:ss} %msg%n")
+                        .build();
+
+                if(config.getBoolean(ROLLING_LOGS)){
+                    RollingFileAppender fileAppender = RollingFileAppender.newBuilder()
+                            .setName("Rolling File Appender")
+                            .withFileName(config.getString(LOG_DIR) + "/LogProxy.log")
+                            .withFilePattern(config.getString(LOG_DIR) + "/$${date:yyyy-MM}/LogProxy-%d{yyyy-MM-dd}-%i.log.gz")
+                            .withPolicy(TimeBasedTriggeringPolicy.newBuilder().withInterval(1).build())
+                            .setConfiguration(logConfig)
+                            .setLayout(logLayout)
+                            .build();
+                    fileAppender.start();
+                    context.getConfiguration().getRootLogger().addAppender(fileAppender, logLevel, null);
+                }else{
+                    FileAppender fileAppender = FileAppender.newBuilder()
+                            .setName("File Appender")
+                            .withFileName(new File(config.getString(LOG_DIR) + "/LogProxy.log").getAbsolutePath())
+                            .setConfiguration(logConfig)
+                            .setLayout(logLayout)
+                            .build();
+                    fileAppender.start();
+                    context.getConfiguration().getRootLogger().addAppender(fileAppender, logLevel, null);
                 }
+
+                context.updateLoggers();
             }
         }
 
         try {
-            CollaboratorLogProxy server = new CollaboratorLogProxy(properties.toJdkProperties());
+            CollaboratorLogProxy server = new CollaboratorLogProxy(config);
             server.start();
         }catch (Exception e){
             e.printStackTrace();
         }
-    }
-
-    private static OrderedProperties getDefaultProperties(){
-        OrderedProperties defaultProperties = new OrderedProperties.OrderedPropertiesBuilder()
-                .withSuppressDateInComment(true).build();
-        defaultProperties.setProperty(COLLABORATOR_SERVER_ADDRESS, "127.0.0.1");
-        defaultProperties.setProperty(COLLABORATOR_HTTP_SERVER_PORT, "81");
-        defaultProperties.setProperty(COLLABORATOR_HTTPS_SERVER_PORT, "444");
-        defaultProperties.setProperty(LOG_PROXY_HTTP_SERVER_PORT, "80");
-        defaultProperties.setProperty(LOG_PROXY_HTTPS_SERVER_PORT, "443");
-        defaultProperties.setProperty(LISTEN_ADDRESS, "0.0.0.0");
-        defaultProperties.setProperty(PRIVATE_KEY_PATH, "/certs/key.pem.pkcs8");
-        defaultProperties.setProperty(CERTIFICATE_PATH, "/certs/cert.crt");
-        defaultProperties.setProperty(INTERMEDIATE_CERTIFICATE_PATH, INTERMEDIATE_CERTIFICATE_DEFAULT);
-        defaultProperties.setProperty(LOG_LEVEL, "INFO");
-
-        return defaultProperties;
     }
 }
